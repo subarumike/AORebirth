243adcb3deb8b296f1bae7631fd9f84746eb98b9
subarumike <102692556+subarumike@users.noreply.github.com>
2026-07-01 14:14:46 -0500

    Add Montroyal content module skeleton

---
+++ AORebirth/Server/ZoneEngine/Core/Playfields/Content/MontroyalContentModule.cs
@@ -0,0 +1,28 @@
+namespace ZoneEngine.Core.Playfields.Content
+{
+    #region Usings ...
+
+    using SmokeLounge.AOtomation.Messaging.GameData;
+
+    #endregion
+
+    public sealed class MontroyalContentModule : IPlayfieldContentModule
+    {
+        private const int MontroyalPlayfieldInstance = 655;
+
+        public bool Supports(Identity playfieldIdentity)
+        {
+            if (playfieldIdentity.Type != IdentityType.Playfield
+                && playfieldIdentity.Type != IdentityType.Playfield2)
+            {
+                return false;
+            }
+
+            return playfieldIdentity.Instance == MontroyalPlayfieldInstance;
+        }
+
+        public void Register(PlayfieldContentRegistration registration)
+        {
+        }
+    }
+}
