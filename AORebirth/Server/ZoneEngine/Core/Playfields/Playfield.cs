706d7a1a4925c948823153723555859decfe4677
subarumike <102692556+subarumike@users.noreply.github.com>
2026-07-01 11:12:45 -0500

    Extract captured Arete robot spawn orchestration

--- AORebirth/Server/ZoneEngine/Core/Playfields/Playfield.cs
+++ AORebirth/Server/ZoneEngine/Core/Playfields/Playfield.cs
@@ -231,8 +231,6 @@ namespace AORebirth.Core.Playfields
 
         private const double CapturedCleaningRobotFollowStopDistance = 0.0;
 
-        private const int PrivateAretePlayfieldInstance = 6553;
-
         private static readonly int[][] CapturedCleaningRobotLootOutcomes =
         {
             new[] { 42620 },
@@ -260,6 +258,9 @@ namespace AORebirth.Core.Playfields
 
         private static readonly NpcPatrolReplayCoordinator NpcPatrolReplay =
             new NpcPatrolReplayCoordinator(CapturedAreteRobotContent);
+
+        private static readonly CapturedAreteRobotSpawnOrchestrator CapturedAreteRobotSpawns =
+            new CapturedAreteRobotSpawnOrchestrator(CapturedAreteRobotContent, NpcPatrolReplay);
 
         private const int UnarmedAttackInfoAmmoCount = -1;
 
@@ -354,7 +355,7 @@ namespace AORebirth.Core.Playfields
 
             this.statels = ResolvePlayfieldStatels(playfieldIdentity);
             this.LoadMobSpawns(playfieldIdentity);
-            this.LoadCapturedAreteMobSpawns(playfieldIdentity);
+            CapturedAreteRobotSpawns.SpawnForPlayfield(this, playfieldIdentity);
             this.LoadVendors(playfieldIdentity);
             this.LoadStaticDynels(playfieldIdentity);
         }
@@ -487,136 +488,6 @@ namespace AORebirth.Core.Playfields
         private static void LogCapturedAreteRobotContent(bool isError, string message)
         {
             LogUtil.Debug(isError ? DebugInfoDetail.Error : DebugInfoDetail.Engine, message);
-        }
-
-        private void LoadCapturedAreteMobSpawns(Identity playfieldIdentity)
-        {
-            if (playfieldIdentity.Instance != PrivateAretePlayfieldInstance)
-            {
-                return;
-            }
-
-            CapturedAreteRobotSpawnDefinition[] spawns = CapturedAreteRobotContent.GetSpawnDefinitions();
-            PlayfieldLifecycleTrace.Record(
-                PlayfieldLifecycleTrace.FlowCapturedAreteRobotSpawn,
-                PlayfieldLifecycleTrace.StageCapturedAreteRobotSpawnRowsLoaded,
-                PlayfieldLifecycleTrace.MessageCapturedAreteRobotSpawnRowsLoaded,
-                playfieldIdentity,
-                PlayfieldLifecycleTrace.FormatCapturedAreteRobotSpawnRowsDetail(
-                    spawns.Length,
-                    CapturedCleaningRobotMonsterData));
-
-            foreach (CapturedAreteRobotSpawnDefinition spawn in spawns)
-            {
-                this.SpawnCapturedAreteCleaningRobot(playfieldIdentity, spawn);
-            }
-        }
-
-        private void SpawnCapturedAreteCleaningRobot(Identity playfieldIdentity, CapturedAreteRobotSpawnDefinition spawn)
-        {
-            var npcController = new NPCController();
-            Character mobCharacter = NonPlayerCharacterHandler.SpawnMobFromTemplate(
-                CombatTestMobArchetype.TemplateHash,
-                playfieldIdentity,
-                new Coordinate { x = spawn.X, y = spawn.Y, z = spawn.Z },
-                new AORebirth.Core.Vector.Quaternion(0, 0, 0, 1),
-                npcController,
-                spawn.Level);
-
-            if (mobCharacter == null)
-            {
-                LogUtil.Debug(
-                    DebugInfoDetail.Error,
-                    string.Format(
-                        "Captured Arete robot spawn failed source=20260629-193121 sourceIdentity=SimpleChar:{0:X8}",
-                        spawn.SourceInstance));
-                return;
-            }
-
-            mobCharacter.Name = CapturedCleaningRobotName;
-            mobCharacter.Playfield = this;
-            CombatTestMobArchetype.Prepare(mobCharacter, CombatTestMobArchetype.MalfunctioningCleaningRobot);
-            SetCapturedMobStat(mobCharacter, StatIds.life, spawn.Health);
-            SetCapturedMobStat(mobCharacter, StatIds.health, spawn.Health);
-            SetCapturedMobStat(mobCharacter, StatIds.level, spawn.Level);
-            SetCapturedMobStat(mobCharacter, StatIds.runspeed, spawn.RunSpeed);
-            mobCharacter.Coordinates(new Coordinate { x = spawn.X, y = spawn.Y, z = spawn.Z });
-            AssignCapturedPatrolWaypoints(mobCharacter, spawn);
-            PlayfieldLifecycleTrace.Record(
-                PlayfieldLifecycleTrace.FlowCapturedAreteRobotSpawn,
-                PlayfieldLifecycleTrace.StageCapturedAreteRobotSpawnCreated,
-                PlayfieldLifecycleTrace.MessageCapturedAreteRobotSpawnCreated,
-                mobCharacter.Identity,
-                PlayfieldLifecycleTrace.FormatCapturedAreteRobotSpawnCreatedDetail(
-                    spawn.SourceInstance,
-                    CapturedCleaningRobotMonsterData,
-                    spawn.Health,
-                    spawn.Level,
-                    spawn.RunSpeed,
-                    spawn.X,
-                    spawn.Y,
-                    spawn.Z,
-                    spawn.PatrolX,
-                    spawn.PatrolY,
-                    spawn.PatrolZ));
-
-            int replaySegmentCount = 0;
-            NpcPatrolReplay.AssignCapturedAreteRobotReplay(
-                spawn.SourceInstance,
-                segments =>
-                {
-                    replaySegmentCount = segments == null ? 0 : segments.Length;
-                    npcController.SetCapturedPatrolReplaySegments(segments);
-                });
-            PlayfieldLifecycleTrace.Record(
-                PlayfieldLifecycleTrace.FlowCapturedAreteRobotSpawn,
-                PlayfieldLifecycleTrace.StageCapturedAreteRobotPatrolReplayAssigned,
-                PlayfieldLifecycleTrace.MessageCapturedAreteRobotPatrolReplayAssigned,
-                mobCharacter.Identity,
-                PlayfieldLifecycleTrace.FormatCapturedAreteRobotPatrolReplayAssignedDetail(
-                    spawn.SourceInstance,
-                    replaySegmentCount));
-
-            mobCharacter.DoNotDoTimers = false;
-            PlayfieldLifecycleTrace.Record(
-                PlayfieldLifecycleTrace.FlowCapturedAreteRobotSpawn,
-                PlayfieldLifecycleTrace.StageCapturedAreteRobotSimpleCharFullUpdateBroadcast,
-                PlayfieldLifecycleTrace.MessageSimpleCharFullUpdate,
-                mobCharacter.Identity,
-                PlayfieldLifecycleTrace.FormatCapturedAreteRobotSimpleCharFullUpdateDetail(spawn.SourceInstance));
-            this.Announce(SimpleCharFullUpdate.ConstructMessage(mobCharacter));
-
-            LogUtil.Debug(
-                DebugInfoDetail.Engine,
-                string.Format(
-                    CultureInfo.InvariantCulture,
-                    "Captured Arete robot spawned source=20260629-193121 sourceIdentity=SimpleChar:{0:X8} serverIdentity={1} pos=({2},{3},{4}) health={5} level={6} runSpeed={7}",
-                    spawn.SourceInstance,
-                    mobCharacter.Identity,
-                    spawn.X,
-                    spawn.Y,
-                    spawn.Z,
-                    spawn.Health,
-                    spawn.Level,
-                    spawn.RunSpeed));
-        }
-
-        private static void AssignCapturedPatrolWaypoints(ICharacter mobCharacter, CapturedAreteRobotSpawnDefinition spawn)
-        {
-            mobCharacter.Waypoints.Clear();
-            mobCharacter.AddWaypoint(
-                new AORebirth.Core.Vector.Vector3(spawn.X, spawn.Y, spawn.Z),
-                false);
-            mobCharacter.AddWaypoint(
-                new AORebirth.Core.Vector.Vector3(spawn.PatrolX, spawn.PatrolY, spawn.PatrolZ),
-                false);
-            mobCharacter.Controller.State = CharacterState.Patrolling;
-        }
-
-        private static void SetCapturedMobStat(ICharacter mobCharacter, StatIds stat, int value)
-        {
-            mobCharacter.Stats[stat].Value = value;
-            mobCharacter.Stats[stat].BaseValue = (uint)value;
         }
 
         #endregion
