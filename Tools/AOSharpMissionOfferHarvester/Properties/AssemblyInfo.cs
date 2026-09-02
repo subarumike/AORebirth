2d0e16635ddca2206c9bde805a8e676c5cbde0a7
subarumike <102692556+subarumike@users.noreply.github.com>
2026-09-02 03:13:44 -0500

    Report mission capture progress every ten rolls

--- Tools/AOSharpMissionOfferHarvester/Properties/AssemblyInfo.cs
+++ Tools/AOSharpMissionOfferHarvester/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("AORebirth Mission Evidence")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8E2D0836-D3AB-4F22-811F-957D3FAAD922")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
